a74579fbadd6d7476a2e8a1e4c2b23f43d8275d9
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-21 10:32:48 +0100

    wip

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,31 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
-
-const getStyles = ({ primary }) => ({
-  header: {
-    padding: '10px',
-    marginTop: '10px',
-    border: `4px solid ${primary}`,
-  },
-  nav: {
-    display: 'flex',
-    justifyContent: 'center',
-  },
-  link: {
-    marginLeft: '10px',
-    marginRight: '10px',
-  },
-});
+// tag functions (ES6)
+const StyledHeader = styled.header`
+  border: 4px solid black;
+  padding: 10px;
+`;
 
 export default function Playground({ primary }) {
-  const styles = getStyles({ primary });
   return (
-    <header style={styles.header}>
-      <nav style={styles.nav}>
-        <a style={styles.link} href="#">Home</a>
-        <a style={styles.link} href="#">About</a>
-        <a style={styles.link} href="#">Blog</a>
+    <StyledHeader>
+      <nav>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Blog</a>
       </nav>
-    </header>
+    </StyledHeader>
   );
 }
+
+// const getStyles = ({ primary }) => ({
+//   header: {
+//     padding: '10px',
+//     marginTop: '10px',
+//     border: `4px solid ${primary}`,
+//   },
+//   nav: {
+//     display: 'flex',
+//     justifyContent: 'center',
+//   },
+//   link: {
+//     marginLeft: '10px',
+//     marginRight: '10px',
+//   },
+// });
