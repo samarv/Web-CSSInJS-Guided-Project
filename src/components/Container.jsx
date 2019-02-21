43b228a813c7bafff15df88253ff0ab61f12fde2
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-21 10:20:52 +0100

    wip

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,10 +3,20 @@ import Playground from './Playground';
 
 
 export default class Container extends React.Component {
+  state = {
+    primary: 'black',
+  }
+
+  setPrimary = primary => this.setState({ primary })
+
   render() {
     return (
       <div className='container'>
-        <Playground />
+        <Playground primary={this.state.primary} />
+
+        <button onClick={() => this.setPrimary('red')}>Red</button>
+        <button onClick={() => this.setPrimary('yellow')}>Yellow</button>
+        <button onClick={() => this.setPrimary('blue')}>Blue</button>
       </div>
     );
   }
--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
 
 
-export default function Playground() {
+const getStyles = ({ primary }) => ({
+  header: {
+    padding: '10px',
+    marginTop: '10px',
+    border: `4px solid ${primary}`,
+  },
+  nav: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  link: {
+    marginLeft: '10px',
+    marginRight: '10px',
+  },
+});
+
+export default function Playground({ primary }) {
+  const styles = getStyles({ primary });
   return (
-    <div>
-      This is the playground component
-    </div>
+    <header style={styles.header}>
+      <nav style={styles.nav}>
+        <a style={styles.link} href="#">Home</a>
+        <a style={styles.link} href="#">About</a>
+        <a style={styles.link} href="#">Blog</a>
+      </nav>
+    </header>
   );
 }
