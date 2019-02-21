1be301b5d9ea9ef5a505c372bae16aef737cfa05
Gabriel Cabrejas <gabriel@Gabriels-MacBook-Pro-2.local>
2019-02-21 09:31:36 +0100

    prepared for live instruction

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { ThemeProvider } from 'styled-components';
 import Container from './components/Container';
 
-const theme = {
-  primary: 'red',
-  secondary: 'orange',
-};
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}><Container /></ThemeProvider>, document.querySelector('#target'),
+  <Container />, document.querySelector('#target'),
 );
--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,84 +1,10 @@
-import React, { useState } from 'react';
-import styled, { keyframes } from 'styled-components';
-import { Button } from 'reactstrap';
-import { invert } from 'polished';
+import React from 'react';
 
-
-const StyledLink = styled.a`
-  margin-left: 5px;
-  margin-right: 5px;
-  font-weight: ${pr => (pr.bold ? '700' : '200')};
-
-  &:hover {
-    color: ${invert('#ab0')};
-  }
-`;
-
-const kf = keyframes`
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const ExtraordinaryLink = styled(StyledLink)`
-  font-size: 20px;
-  animation: ${kf} 2s linear infinite;
-
-  ::before {
-    content: '😛';
-  }
-`;
-
-const StyledHeader = styled.header`
-  border: 3px solid ${pr => pr.theme.primary};
-  border-radius: ${pr => pr.radius}px;
-  padding: 10px;
-  margin-top: 20px;
-
-  nav {
-    display: flex;
-    justify-content: center;
-  }
-
-  @media (max-width: 400px) {
-    & {
-      border: 13px solid ${pr => pr.theme.secondary};
-    }
-  }
-`;
 
 export default function Playground() {
-  const [radius, setRadius] = useState(1);
-  const makeBiggerRadius = () => setRadius(radius * 2);
-  const makeSmallerRadius = () => setRadius(radius / 2);
-
   return (
-    <StyledHeader
-      radius={radius}
-    >
-      <nav>
-        <StyledLink id='specialLink' hoverColor='orange' href="#">Home</StyledLink>
-        <ExtraordinaryLink hoverColor='green' href="#">Blog</ExtraordinaryLink>
-        <StyledLink bold hoverColor='yellow' href="#">About us</StyledLink>
-      </nav>
-      <button onClick={makeBiggerRadius}>More Radius!</button>
-      <Button color="danger" onClick={makeSmallerRadius}>Less Radius!</Button>
-    </StyledHeader>
+    <div>
+      This is the playground component
+    </div>
   );
 }
-
-// const getStyles = ({ primaryColor }) => ({
-//   header: {
-//     border: `3px solid ${primaryColor}`,
-//     padding: '10px',
-//     marginTop: '20px',
-//   },
-//   nav: {
-//     display: 'flex',
-//     justifyContent: 'center',
-//   },
-//   link: {
-//     marginLeft: '5px',
-//     marginRight: '5px',
-//   },
-// });
