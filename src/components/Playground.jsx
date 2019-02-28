d66739a68b08b5fd99a5172e9321c464cbc7f38f
samarv <samarvir1996@gmail.com>
2019-02-28 13:11:41 +0530

    novina guided demo

--- src/components/Playground.jsx
+++ src/components/Playground.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 
+const getStyle = () => ({
+  header: {
+    padding: '10px',
+    marginTop: '10px',
+    border: '4px solid black',
+  },
+  nav: {
+    border: '5px solid red',
+  },
+});
 
 export default function Playground() {
+  let styles = getStyle();
   return (
-    <div>
-      This is the playground component
+    <div style={styles.header}>
+      parent'splayground component
+
+      <div style={styles.nav}>
+        child's playground component
+      </div>
     </div>
   );
 }
